c2741da30b596cd859ac26075ba0406099818dfa
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-05-02 12:26:56 -0700

    fix: home page mobile layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,11 +30,16 @@ export default function RootLayout({
             <main
               className={cn(
                 "flex w-full flex-col items-center justify-center md:w-[calc(100%-calc(var(--spacing)*24))]",
-                "pb-16 md:pb-0 md:m-2",
+                "pb-16 md:m-2 md:pb-0",
                 "bg-[var(--ctp-surface1)] md:rounded-xl md:shadow",
               )}
             >
-              <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+              <div
+                className={cn(
+                  "container flex h-full flex-col items-center gap-12 md:justify-center ", 
+                  "md:px-4  py-1 md:py-16",
+                )}
+              >
                 {children}
               </div>
             </main>

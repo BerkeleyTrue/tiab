f3f3a7198fed171787d2c655b8c1561bf8a89217
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-05-03 21:02:03 -0700

    feat: more flake build improvements

--- flake.nix
+++ flake.nix
@@ -17,30 +17,32 @@
         nodejs = pkgs.nodejs_20;
 
         # PNPM version from packageManager field
-        pnpm = pkgs.nodePackages.pnpm;
+        pnpm = pkgs.pnpm_10;
 
         # Project name from package.json
         name = "tiab";
       in
       {
         # Package definition
-        packages.default = pkgs.stdenv.mkDerivation {
+        packages.default = pkgs.stdenv.mkDerivation (finalAttrs: {
           pname = name;
           version = "0.1.0";
           src = ./.;
 
-          buildInputs = [
+          nativeBuildInputs = [
             nodejs
             pnpm
+            pnpm.configHook
           ];
 
           buildPhase = ''
-            export HOME=$(mktemp -d)
-            pnpm install --frozen-lockfile
-            pnpm build
+            runHook preBuild
+            pnpm run build
+            runHook postBuild
           '';
 
           installPhase = ''
+            runHook preInstall
             mkdir -p $out
             cp -r .next $out/
             cp -r public $out/
@@ -50,19 +52,29 @@
             # Create a simple wrapper script to start the application
             mkdir -p $out/bin
             cat > $out/bin/${name} << EOF
-            #!/bin/sh
-            cd $out
-            exec ${nodejs}/bin/node $out/node_modules/next/dist/bin/next start
+              #!/bin/sh
+              cd $out
+              exec ${nodejs}/bin/node $out/node_modules/next/dist/bin/next start
             EOF
             chmod +x $out/bin/${name}
+            runHook postInstall
           '';
-        };
+
+          pnpmDeps = pnpm.fetchDeps {
+            inherit (finalAttrs) pname version src;
+            hash = "sha256-sz0jOS2sV+rYrdHgIvYarL6+0hhyj5vZA2rtMDcrkHE="; # Replace with actual hash
+          };
+          
+          meta = {
+            mainProgram = name;
+          };
+        });
 
         # App definition for running with `nix run`
         apps.default = {
           type = "app";
+          description = "Tiab application - A personal storage solution";
           program = "${self.packages.${system}.default}/bin/${name}";
         };
       });
 }
-
